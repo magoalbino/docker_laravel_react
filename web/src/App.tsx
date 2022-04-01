ba2d50dd41f0577a005fab3d2b8a4c056edaba69
Daniel <magoalbino@gmail.com>
2022-04-01 18:24:33 -0300

    Projeto básico

---
+++ web/src/App.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {api} from "./services/api";
+
+function App() {
+  const [test, setTest] = useState('')
+
+  useEffect(() => {
+    api.get("test").then(
+        response => setTest(response.data)
+    )
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Response: { test || 'nada ainda =(' }
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
